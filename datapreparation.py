1070c8687bd7bb3baf299ee569b2ca1776978c67
Shreyas Shankar <shreyas.shankar23@gmail.com>
2017-04-08 16:05:00 -0700

    Added code to parse new JSON files

--- datapreparation.py
+++ datapreparation.py
@@ -4,6 +4,44 @@ import json
 
 # Other data preparation
 # convert to small case.
+
+
+def getWordCount(file):
+    jsonfile = open("json/24_CellPhones.json")
+    jsondata = json.load(jsonfile)
+    for prod in jsondata.products:
+        if details in prod:
+            for fullreview in prod['details']:
+                star = full_review['rate'][0]
+                if 'comments' in full_review:
+                    with open(bad_reviews.txt, 'w') as badreviewfile:
+                        comment = full_review['comments'].lower()
+                        words = comment.split()
+                        if star == '1' or star == '2':
+                            for word in words:
+                                if word in negative_reviews:
+                                    negative_reviews[word] = negative_reviews[word] + 1
+                                else:
+                                    negative_reviews[word] = 1
+                        badreviewfile.write(comment + '\n')
+                    elif star == '4' or star == '5':
+                        with open(good_review.txt, 'w') as goodreviewfile:
+                            for word in words:
+                                if word in positive_reviews:
+                                    positive_reviews[word]  = positive_reviews[word] + 1
+                                else:
+                                    positive_reviews[word] = 1
+                            goodreviewfile.write(comment + '\n')
+                    else:
+                        with open(neutral_reviews)
+                        for word in words:
+                            if word in neutral_reviews:
+                                neutral_reviews[word] = neutral_reviews[word] + 1
+                            else:
+                                neutral_reviews[word] = 1
+
+
+
 jsonfile = open("json/run_results.json")
 jsondata = json.load(jsonfile)
 positive_reviews = {}
@@ -32,6 +70,13 @@ for full_review in jsondata['data']:
                     neutral_reviews[word] = neutral_reviews[word] + 1
                 else:
                     neutral_reviews[word] = 1
+
+
+getWordCount(json/24_CellPhones.json)
+getWordCount(json/27_PCs.json)
+getWordCount(tablets_data.json)
+
+
 
 print positive_reviews
 print "\n\n\n"
