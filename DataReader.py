fdb34489b5a9c911f386da7fc3ace6d9f6d95cd7
likith <likithym@gmail.com>
2017-04-09 19:54:37 -0700

    Added word model to Language Model

--- DataReader.py
+++ DataReader.py
@@ -1,267 +1,203 @@
-import tensorflow as tf
-from tensorflow.contrib import rnn
+# pylint: disable=unused-import,g-bad-import-order
+
+"""Utilities for parsing Language Model text files."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import os
+import sys
+import time
+
+import tensorflow.python.platform
+
 import numpy as np
-from DataReader import CharReader
+import tensorflow as tf
 
-flags = tf.flags
-logging = tf.logging
+from tensorflow.python.platform import gfile
 
-flags.DEFINE_string("save_model_path", None, "directory to output the model")
-flags.DEFINE_string("data_path", "TrumpBSQuotes.txt", "The path point to the training and testing data")
-flags.DEFINE_integer("ckpt", 1, "Checkpoint after this many steps (default: 100)")
-#flags.DEFINE_string("model", "word_model", "choose the model")
-flags.DEFINE_string("model", "char_model", "choose the model")
+class CharReader(object):
 
+  def __init__(self, data_path, val_to_train_ratio=5):
 
-FLAGS = flags.FLAGS
+    self.raw_data = open(data_path, 'r').read()
 
-class LanguageModel(object):
+    self.val_to_train_ratio = val_to_train_ratio
 
-    def __init__(self, config, is_training=True):
-        self.config = config
-        self.input_x_seq = tf.placeholder(tf.int32, [self.config.batch_size, self.config.sequence_size], "input_x_seq")
-        self.output_y_seq = tf.placeholder(tf.int32, [self.config.batch_size, self.config.sequence_size], "output_y_seq")
+    #Build Vocabulary and Dictionaries
+    counter = collections.Counter(self.raw_data)
+    count_pairs = sorted(counter.items(), key=lambda x: -x[1])
+    self.unique_tokens, _ = list(zip(*count_pairs))
+    self.token_to_id = dict(zip(self.unique_tokens, range(len(self.unique_tokens))))
+    self.vocab_size = len(self.unique_tokens)
 
-        with tf.device("/cpu:0"):
-            embedding = tf.get_variable("embedding", [self.config.vocab_size, self.config.hidden_size])
-            embedding_looked_up = tf.nn.embedding_lookup(embedding, self.input_x_seq)
-            inputs = tf.split(embedding_looked_up, self.config.sequence_size, 1)
-            input_tensors_list = [tf.squeeze(i, [1]) for i in inputs]
+    #print len(self.unique_tokens)
 
-        def lstm_cell():
-            return tf.contrib.rnn.BasicLSTMCell(self.config.hidden_size, state_is_tuple=True)
-
-        single_cell = lstm_cell
-
-        #if is_training and self.config.keep_prob < 1:
-        #    def single_cell():
-        #        return tf.nn.rnn_cell.DropoutWrapper(lstm_cell(), output_keep_prob=self.config.keep_prob)
-
-        self.cells =  tf.contrib.rnn.MultiRNNCell([single_cell()] * self.config.num_layers, state_is_tuple=True)
-        self._initial_state = self.cells.zero_state(self.config.batch_size, tf.float32)
-
-        #logits for softmax
-        hidden_layer_output, last_state = tf.contrib.rnn.static_rnn(self.cells, input_tensors_list, initial_state=self._initial_state)
-        hidden_layer_output = tf.reshape(tf.concat(hidden_layer_output, 1), [-1, self.config.hidden_size])
-        self._logits = tf.nn.xw_plus_b(hidden_layer_output, tf.get_variable("softmax_w", [self.config.hidden_size, self.config.vocab_size]), tf.get_variable("softmax_b", [self.config.vocab_size]))
-        self._predictions = tf.nn.softmax(self.logits)
-
-        #loss function
-        loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example([self._logits], [tf.reshape(self.output_y_seq, [-1])], [tf.ones([self.config.batch_size * self.config.sequence_size])], self.config.vocab_size)
-        self._cost = tf.div(tf.reduce_sum(loss), self.config.batch_size)
-
-        self._final_state = last_state
-
-    def GradientDescentOptimize(self):
-        self._learning_rate = tf.Variable(0.0, trainable=False)
-
-        training_vars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, training_vars),self.config.max_grad_norm)
-        optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        self.run_gradient_descent_optimizer = optimizer.apply_gradients(zip(grads, training_vars))
+    #Convert Raw tokens to digits
+    self.data_as_ids = []
+    for id, token in enumerate(self.raw_data):
+      if token in self.token_to_id.keys():
+        self.data_as_ids.append(self.token_to_id[token])
+      else:
+        self.data_as_ids.append(0)
 
 
-    def assign_learning_rate(self, session, lr_value):
-        session.run(tf.assign(self.learning_rate, lr_value))
-
-    @property
-    def input_seq(self):
-        return self.input_x_seq
-
-    @property
-    def target_seq(self):
-        return self.output_y_seq
-
-    @property
-    def initial_state(self):
-        return self._initial_state
-
-    @property
-    def cost(self):
-        return self._cost
-
-    @property
-    def final_state(self):
-        return self._final_state
-
-    @property
-    def learning_rate(self):
-        return self._learning_rate
-
-    @property
-    def gradient_desc_training_op(self):
-        return self.run_gradient_descent_optimizer
-
-    @property
-    def prediction_softmax(self):
-        return self._predictions
-
-    @property
-    def logits(self):
-        return self._logits
-
-def get_config():
-    if FLAGS.model == "word_model":
-        return WordModel()
-    elif FLAGS.model == "word_model_dp":
-        return WordModelWithDropuout()
-    elif FLAGS.model == "char_model":
-        return CharModel()
-    elif FLAGS.model == "char_model_dp":
-        return CharModelWithDropout()
-    else:
-        raise ValueError("Invalid model: %s", FLAGS.model)
-
-def get_reader():
-    if FLAGS.model == "word_model" or FLAGS.model == "word_model_dp":
-        pass #implement word based reader
-    elif FLAGS.model == "char_model" or FLAGS.model == "char_model_dp":
-        return CharReader(FLAGS.data_path)
-    else:
-        raise ValueError("Invalid model: %s", FLAGS.model)
-
-def main(unused_args):
-
-    #reader = DataReader(FLAGS.data_path)
-    reader = get_reader()
-
-    with tf.Graph().as_default(), tf.Session() as session:
-        config = get_config()
-        config.vocab_size = reader.vocab_size
-
-        initializer = tf.random_uniform_initializer(-config.init_scale, config.init_scale)
-
-        with tf.variable_scope("LanguageModel", reuse=None, initializer=initializer):
-          training_model = LanguageModel(config)
-          training_model.GradientDescentOptimize()
-        with tf.variable_scope("LanguageModel", reuse=True, initializer=initializer):
-          eval_config = get_config()
-          eval_config.vocab_size = reader.vocab_size
-          eval_config.batch_size = 1
-          eval_config.num_steps = 1
-          prediction_model = LanguageModel(eval_config)
-
-          tf.global_variables_initializer().run()
-
-        for epoch in range(config.max_max_epoch):
-            accumulated_costs = 0.0
-            accumulated_seq_count = 0
-            current_state = session.run(training_model.initial_state)
-
-            learning_rate_decay = config.lr_decay ** max(epoch - config.max_epoch, 0.0)
-            training_model.assign_learning_rate(session, config.learning_rate * learning_rate_decay)
-
-            lowest_perplexity = 2000
-
-            for seq_counter, (x, y) in enumerate(reader.generateXYPairs(reader.get_training_data(), training_model.config.batch_size, training_model.config.sequence_size)):
-                feed_dict = {training_model.input_x_seq: x, training_model.output_y_seq: y, training_model.initial_state: current_state}
-
-                cost, current_state, _ = session.run([training_model.cost, training_model.final_state, training_model.gradient_desc_training_op], feed_dict)
-                accumulated_costs += cost
-                accumulated_seq_count += training_model.config.sequence_size
-                perplexity =  np.exp(accumulated_costs / accumulated_seq_count)
-
-                if  seq_counter != 0 and seq_counter % tf.flags.FLAGS.ckpt == 0:
-                  print("Epoch %d, Perplexity: %.3f" % (epoch, perplexity))
-
-                  if perplexity < lowest_perplexity:
-                    lowest_perplexity = perplexity
-                    #get_prediction(prediction_model, bs_reader, session, 500, ['T','h','e',' '])
-                    get_prediction(prediction_model, reader, session, 500, [' '])
-
-    session.close()
+    #Create Data Indexes
+    data_size = len(self.data_as_ids)
+    training_data_ratio = 100 - (self.val_to_train_ratio*2)
+    self.train_data_max_index = (data_size * training_data_ratio)//100
+    self.valid_data_min_index = self.train_data_max_index+1
+    self.valid_data_max_index = ((data_size * self.val_to_train_ratio)//100)+self.train_data_max_index
+    self.test_data_min_index = self.valid_data_max_index+1
 
 
-def get_prediction(model, reader, session, total_tokens, output_tokens = [' ']):
+  def print_data_info(self):
+    print('----------------------------------------')
+    print('Data total tokens: %d tokens' % (len(self.raw_data)))
+    print('Data vocabulary size: %d tokens' % (len(self.unique_tokens)))
+    print('Training Data total tokens: %d tokens' % (len(self.get_training_data())))
+    print('Validation Data total tokens: %d tokens' % (len(self.get_validation_data())))
+    print('Test Data total tokens: %d tokens' % (len(self.get_test_data())))
+    print('----------------------------------------')
 
-  state = session.run(model.cells.zero_state(1, tf.float32))
+  def _read_words(self, filename):
+    with gfile.GFile(filename, "r") as f:
+      return f.read().replace("\n", " <eos> ").split()
 
-  for token_count in range(total_tokens):
-      next_token = output_tokens[token_count]
-      input = np.full((model.config.batch_size, model.config.sequence_size), reader.token_to_id[next_token], dtype=np.int32)
-      feed = {model.input_x_seq: input, model.initial_state:state}
-      [prediction_softmax, state] =  session.run([model.prediction_softmax, model.final_state], feed)
 
-      if (len(output_tokens) -1) <= token_count:
-          accumulated_sum = np.cumsum(prediction_softmax[0])
-          currentTokenId = (int(np.searchsorted(accumulated_sum, np.random.rand(1))))
-          next_token = reader.unique_tokens[currentTokenId]
-          output_tokens.append(next_token)
 
-  output_sentence = " "
-  for token in output_tokens:
-    output_sentence+=token
-  print('---- Prediction: \n %s \n----' % (output_sentence))
 
-class WordModel(object):
-    init_scale = 0.1
-    #learning_rate = 1.0
-    learning_rate = 0.002
-    max_grad_norm = 5
-    num_layers = 2
-    num_steps = 20
-    sequence_size = 20
-    #hidden_size = 200
-    hidden_size = 128
-    #max_epoch = 4
-    max_epoch = 1
-    #max_max_epoch = 13
-    max_max_epoch = 10000
-    keep_prob = 1.0
-    #lr_decay = 0.5
-    lr_decay = 0.97
-    batch_size = 20
-    vocab_size = 10000
+  def convertDigitsToText(self, token_predicted_per_batch,  first_token=" "):
 
-class WordModelWithDropuout(object):
-    init_scale = 0.1
-    learning_rate = 1.0
-    max_grad_norm = 5
-    num_layers = 2
-    num_steps = 20
-    sequence_size = 20
-    hidden_size = 200
-    max_epoch = 4
-    max_max_epoch = 13
-    keep_prob = 1.0
-    lr_decay = 0.5
-    batch_size = 20
-    vocab_size = 10000
+    for token_predicted_per_batch_item in token_predicted_per_batch:
+      first_token.join(self.id_to_token[token_predicted_per_batch_item] )
 
-class CharModel(object):
-    init_scale = 0.1
-    #learning_rate = 1.0
-    learning_rate = 0.002
-    max_grad_norm = 5
-    num_layers = 2
-    num_steps = 20
-    sequence_size = 20
-    #hidden_size = 200
-    hidden_size = 128
-    #max_epoch = 4
-    max_epoch = 1
-    #max_max_epoch = 13
-    max_max_epoch = 10000
-    keep_prob = 1.0
-    #lr_decay = 0.5
-    lr_decay = 0.97
-    batch_size = 20
-    vocab_size = 10000
+    return first_token
 
-class CharModelWithDropout(object):
-    init_scale = 0.1
-    learning_rate = 1.0
-    max_grad_norm = 5
-    num_layers = 2
-    num_steps = 20
-    sequence_size = 20
-    hidden_size = 200
-    max_epoch = 4
-    max_max_epoch = 13
-    keep_prob = 1.0
-    lr_decay = 0.5
-    batch_size = 20
-    vocab_size = 10000
 
-if __name__ == "__main__":
-  tf.app.run()
+
+  def get_training_data(self):
+    return self.data_as_ids[0:self.train_data_max_index]
+
+  def get_validation_data(self):
+    return self.data_as_ids[self.valid_data_min_index:self.valid_data_max_index]
+
+  def get_test_data(self):
+    length = len(self.data_as_ids)-1
+    return self.data_as_ids[self.test_data_min_index:length]
+
+  def generateXYPairs(self, raw_data, batch_size, num_steps):
+    raw_data = np.array(raw_data, dtype=np.int32)
+
+    batch_len = len(raw_data) // batch_size
+    data = np.zeros([batch_size, batch_len], dtype=np.int32)
+    for i in range(batch_size):
+      data[i] = raw_data[batch_len * i:batch_len * (i + 1)]
+
+    for i in range((batch_len - 1) // num_steps):
+      x = data[:, i*num_steps:(i+1)*num_steps]
+      y = data[:, i*num_steps+1:(i+1)*num_steps+1]
+      yield (x, y)
+
+
+
+  def limit_data_size(self, original_data, max_size=None):
+    if max_size==None:
+      return original_data
+
+    print("Limiting Input data size to:  %d tokens" % (max_size))
+    return original_data[0:max_size]
+
+
+class WordReader(object):
+
+  def __init__(self, data_path, val_to_train_ratio=5):
+
+    self.raw_data = open(data_path, 'r').read()
+
+    self.val_to_train_ratio = val_to_train_ratio
+
+    #Build Vocabulary and Dictionaries
+    words_data = self._read_words(data_path)
+    words_data.append('')
+    counter = collections.Counter(words_data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    self.unique_tokens, _ = list(zip(*count_pairs))
+    self.token_to_id = dict(zip(self.unique_tokens, range(len(self.unique_tokens))))
+    self.vocab_size = len(self.unique_tokens)
+
+    #print len(self.unique_tokens)
+
+    #Convert Raw tokens to digits
+    self.data_as_ids = [self.token_to_id[word] for word in words_data if word in self.token_to_id]
+    # for id, token in enumerate(self.raw_data):
+    #   if token in self.token_to_id:
+    #     self.data_as_ids.append(self.token_to_id[token])
+    #   else:
+    #     self.data_as_ids.append(0)
+
+
+    #Create Data Indexes
+    data_size = len(self.data_as_ids)
+    training_data_ratio = 100 - (self.val_to_train_ratio*2)
+    self.train_data_max_index = (data_size * training_data_ratio)//100
+    self.valid_data_min_index = self.train_data_max_index+1
+    self.valid_data_max_index = ((data_size * self.val_to_train_ratio)//100)+self.train_data_max_index
+    self.test_data_min_index = self.valid_data_max_index+1
+
+
+  def print_data_info(self):
+    print('----------------------------------------')
+    print('Data total tokens: %d tokens' % (len(self.raw_data)))
+    print('Data vocabulary size: %d tokens' % (len(self.unique_tokens)))
+    print('Training Data total tokens: %d tokens' % (len(self.get_training_data())))
+    print('Validation Data total tokens: %d tokens' % (len(self.get_validation_data())))
+    print('Test Data total tokens: %d tokens' % (len(self.get_test_data())))
+    print('----------------------------------------')
+
+  def _read_words(self, filename):
+    with gfile.GFile(filename, "r") as f:
+       #return f.read().decode("utf-8").replace("\n", "<eos>").split()
+      return f.read().replace("\n", " <eos> ").split()
+
+
+  def convertDigitsToText(self, token_predicted_per_batch,  first_token=" "):
+
+    for token_predicted_per_batch_item in token_predicted_per_batch:
+      first_token.join(self.id_to_token[token_predicted_per_batch_item] )
+
+    return first_token
+
+
+  def get_training_data(self):
+    return self.data_as_ids[0:self.train_data_max_index]
+
+  def get_validation_data(self):
+    return self.data_as_ids[self.valid_data_min_index:self.valid_data_max_index]
+
+  def get_test_data(self):
+    length = len(self.data_as_ids)-1
+    return self.data_as_ids[self.test_data_min_index:length]
+
+  def generateXYPairs(self, raw_data, batch_size, num_steps):
+    raw_data = np.array(raw_data, dtype=np.int32)
+
+    batch_len = len(raw_data) // batch_size
+    data = np.zeros([batch_size, batch_len], dtype=np.int32)
+    for i in range(batch_size):
+      data[i] = raw_data[batch_len * i:batch_len * (i + 1)]
+
+    for i in range((batch_len - 1) // num_steps):
+      x = data[:, i*num_steps:(i+1)*num_steps]
+      y = data[:, i*num_steps+1:(i+1)*num_steps+1]
+      yield (x, y)
+
+
+
+  def limit_data_size(self, original_data, max_size=None):
+    if max_size==None:
+      return original_data
+
+    print("Limiting Input data size to:  %d tokens" % (max_size))
+    return original_data[0:max_size]
